77ad5703b4fabc256a1780729eff25e5c427df3c
AlexeyFroloff <45922538+AlexeyFroloff@users.noreply.github.com>
2023-04-12 22:35:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ from scipy import stats
 
 chat_id = 423200009 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.03
     stat, p_value = stats.ttest_ind(x, y, alternative='two-sided')
     return ( p_value < alpha)
