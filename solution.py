f86cfdf6a15ec58f83ec8d82154bdce19445e8fa
AlexeyFroloff <45922538+AlexeyFroloff@users.noreply.github.com>
2023-04-12 22:24:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 423200009 # Ваш chat ID, не меняйте название переменной
 
 def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    stat, p_value = stats.ttest_ind(x, y, alternative='two-sided')
+    return ( p_value < alpha)
